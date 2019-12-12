c4db5987093c0d30c851d09c8c97b1a46c10bada
James Livesey <jlivesey22@norwich-school.org.uk>
2019-12-12 09:57:19 +0000

    Add header effects and other cool effects

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ $(function() {
     firebase.database().ref("users").orderByChild("score").limitToLast(20).on("value", function(snapshot) {
         $(".highscoreTableData").html("");
 
+        var delayValue = 0;
+
         snapshot.forEach(function(childSnapshot) {
-            $(".highscoreTableData").prepend(
-                $("<tr>").append([
-                    $("<td>").text(childSnapshot.val().name),
-                    $("<td>").text(childSnapshot.val().score)
-                ])
-            );
+            setTimeout(function() {
+                $(".highscoreTableData").prepend(
+                    $("<tr>").append([
+                        $("<td>").text(childSnapshot.val().name),
+                        $("<td>").text(childSnapshot.val().score)
+                    ])
+                ); 
+            }, delayValue);
+
+            delayValue += 100;
         });
     });
 });
