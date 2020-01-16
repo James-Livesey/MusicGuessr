06092f43a47412b9bf528685850f19b044c175b4
James Livesey <jlivesey22@norwich-school.org.uk>
2020-01-16 10:09:17 +0000

    Add working user account pages

---
+++ account.js
@@ -0,0 +1,74 @@
+var currentUser = {
+    uid: null,
+    name: null
+};
+
+function checkUsername(username) {
+    return username.length >= 5 && username.length <= 20 && username.indexOf(" ") < 0;
+}
+
+function signIn() {
+    firebase.auth().signInWithEmailAndPassword($("#email").val(), $("#password").val()).catch(function(error) {
+        $("#accountError").text(error.message);
+    });
+}
+
+function signUp() {
+    if ($("#password").val() == $("#retypedPassword").val() && checkUsername($("#username").val())) {
+        firebase.auth().createUserWithEmailAndPassword($("#email").val(), $("#password").val()).catch(function(error) {
+            $("#accountError").text(error.message);
+        });
+    } else if (!checkUsername($("#username").val())) {
+        $("#accountError").text("Your username must contain between 5 to 20 characters and must not contain spaces");
+    } else {
+        $("#accountError").text("Make sure that both passwords you typed are the same");
+    }
+}
+
+function signOut() {
+    firebase.auth().signOut();
+}
+
+$(function() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in
+
+            currentUser.uid = user.uid;
+
+            if ($("body").attr("data-account") == "signedOut") {
+                window.location.href = "index.html";
+            }
+
+            firebase.database().ref("users/" + currentUser.uid + "/name").once("value", function(snapshot) {
+                var username = snapshot.val();
+                
+                if (username == null) {
+                    firebase.database().ref("users/" + currentUser.uid).set({
+                        name: $("#username").val(),
+                        score: 0
+                    }).then(function() {
+                        window.location.href = "index.html";
+                    });
+                } else {
+                    $(".accountName").text(snapshot.val());
+                    $(".signedIn").show();
+                    $(".signedOut").hide();
+                }
+            });
+        } else {
+            // User is signed out
+
+            currentUser.uid = null;
+            currentUser.name = null;
+
+            if ($("body").attr("data-account") == "signedIn") {
+                window.location.href = "index.html";
+            }
+
+            $(".accountName").text("User");
+            $(".signedIn").hide();
+            $(".signedOut").show();
+        }
+    });
+});
